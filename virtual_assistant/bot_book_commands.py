737dad46e5bdd9e724ac82f1fe31df4137ba6639
zharuk.alex <zzharuk@gmail.com>
2023-11-01 14:52:13 +0200

    added remove_birthday method; added call method book.add_record() after record was changed for store data in db; var name rez changed by result

--- virtual_assistant/bot_book_commands.py
+++ virtual_assistant/bot_book_commands.py
@@ -1,4 +1,4 @@
-from errors import input_error, ValueNotValid
+from errors import input_error 
 from address_book import Record
 
 
@@ -30,6 +30,7 @@ def change_contact(args, book):
     name, old_phone, new_phone = args
     if name in book.keys():
         book[name].edit_phone(old_phone, new_phone)
+        book.add_record(book[name])
         return "Contact updated."
     else:
         raise KeyError
@@ -57,6 +58,7 @@ def phone_delete(args, book):
     name, phone = args
     if name in book.keys():
         book[name].remove_phone(phone)
+        book.add_record(book[name])
         return "Phone deleted."
     else:
         raise KeyError
@@ -67,10 +69,10 @@ def set_email(args, book):
     name, email = args
     record = book.find(name)
     if record is not None:
-        rez = record.set_email(email)
-        if rez:
+        result = record.set_email(email)
+        if result:
             book.add_record(record)
-        return rez
+        return result
     else:
         raise KeyError
 
@@ -80,10 +82,10 @@ def remove_email(args, book):
     name, = args
     record = book.find(name)
     if record is not None:
-        rez = record.remove_email()
-        if rez:
+        result = record.remove_email()
+        if result:
             book.add_record(record)
-        return rez
+        return result
     else:
         raise KeyError
 
@@ -95,10 +97,10 @@ def set_address(args, book):
         record = book.find(name)
 
         if record is not None:
-            rez = record.set_address(address)
-            if rez:
+            result = record.set_address(address)
+            if result:
                 book.add_record(record)
-            return rez
+            return result
         return f'"{name}" not found'
     except Exception as e:
         return e 
@@ -109,10 +111,10 @@ def remove_address(args, contacts):
     record = contacts.find(name)
 
     if record is not None:
-        rez = record.remove_address()
-        if rez:
+        result = record.remove_address()
+        if result:
             contacts.add_record(record)
-        return rez
+        return result
     return f'"{name}" not found'
  
 
@@ -122,10 +124,22 @@ def add_birthday(args, book):
     name, birthday = args
     if name in book.keys():
         book[name].add_birthday(birthday)
+        book.add_record(book[name])
         return "Birthday added to contact."
     else:
         raise KeyError
 
+@input_error
+def delete_birthday(args, book):
+    name, = args
+    if name in book.keys() and book[name].birthday is None:
+        return f"{name} doesn't have birthday"
+    elif name in book.keys() and book[name].birthday is not None:
+        book[name].remove_birthday()
+        book.add_record(book[name])
+        return "Birthday was deleted."
+    else:
+        raise ValueError
 
 @input_error
 def show_birthday(args, book):
